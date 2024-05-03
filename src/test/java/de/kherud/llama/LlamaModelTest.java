d34a3cd2721675c0c3a04cc83c8b5a6b29b1ad48
Jean-Raffi Nazareth <jr-nazareth@inzh.fr>
2024-05-03 14:14:24 +0200

    Upgrade llama model test details:    - Change indentation format    - add property model.path for use external project model

--- src/test/java/de/kherud/llama/LlamaModelTest.java
+++ src/test/java/de/kherud/llama/LlamaModelTest.java
@@ -10,139 +10,144 @@ import org.junit.Test;
 
 public class LlamaModelTest {
 
-	private static final String prefix = "def remove_non_ascii(s: str) -> str:\n    \"\"\" ";
-	private static final String suffix = "\n    return result\n";
-	private static final int nPredict = 10;
+    private static final String prefix = "def remove_non_ascii(s: str) -> str:\n    \"\"\" ";
+    private static final String suffix = "\n    return result\n";
+    private static final int nPredict = 10;
 
-	private static LlamaModel model;
+    private static LlamaModel model;
 
-	@BeforeClass
-	public static void setup() {
-		model = new LlamaModel(
-				new ModelParameters()
-						.setModelFilePath("models/codellama-7b.Q2_K.gguf")
-//						.setModelUrl("https://huggingface.co/TheBloke/CodeLlama-7B-GGUF/resolve/main/codellama-7b.Q2_K.gguf")
-						.setNGpuLayers(43)
-						.setEmbedding(true)
-		);
-	}
+    @BeforeClass
+    public static void setup() {
+        String modelPath = System.getProperty("model.path");
 
-	@AfterClass
-	public static void tearDown() {
-		if (model != null) {
-			model.close();
-		}
-	}
+        ModelParameters modelParameters = new ModelParameters();
+        modelParameters.setNGpuLayers(43).setEmbedding(true);
 
-	@Test
-	public void testGenerateAnswer() {
-		Map<Integer, Float> logitBias = new HashMap<>();
-		logitBias.put(2, 2.0f);
-		InferenceParameters params = new InferenceParameters(prefix)
-				.setTemperature(0.95f)
-				.setStopStrings("\"\"\"")
-				.setNPredict(nPredict)
-				.setTokenIdBias(logitBias);
+        if (modelPath == null) {
+            modelParameters.setModelFilePath("models/codellama-7b.Q2_K.gguf");
+            //modelParameters.setModelUrl("https://huggingface.co/TheBloke/CodeLlama-7B-GGUF/resolve/main/codellama-7b.Q2_K.gguf");               
+        } else {
+            modelParameters.setModelFilePath(modelPath);
+        }
+        model = new LlamaModel(modelParameters);
+    }
 
-		int generated = 0;
-		for (LlamaModel.Output ignored : model.generate(params)) {
-			generated++;
-		}
-		// todo: currently, after generating nPredict tokens, there is an additional empty output
-		Assert.assertTrue(generated > 0 && generated <= nPredict + 1);
-	}
+    @AfterClass
+    public static void tearDown() {
+        if (model != null) {
+            model.close();
+        }
+    }
 
-	@Test
-	public void testGenerateInfill() {
-		Map<Integer, Float> logitBias = new HashMap<>();
-		logitBias.put(2, 2.0f);
-		InferenceParameters params = new InferenceParameters("")
-				.setInputPrefix(prefix)
-				.setInputSuffix(suffix)
-				.setTemperature(0.95f)
-				.setStopStrings("\"\"\"")
-				.setNPredict(nPredict)
-				.setTokenIdBias(logitBias)
-				.setSeed(42);
+    @Test
+    public void testGenerateAnswer() {
+        Map<Integer, Float> logitBias = new HashMap<>();
+        logitBias.put(2, 2.0f);
+        InferenceParameters params = new InferenceParameters(prefix)
+                .setTemperature(0.95f)
+                .setStopStrings("\"\"\"")
+                .setNPredict(nPredict)
+                .setTokenIdBias(logitBias);
 
-		int generated = 0;
-		for (LlamaModel.Output ignored : model.generate(params)) {
-			generated++;
-		}
-		Assert.assertTrue(generated > 0 && generated <= nPredict + 1);
-	}
+        int generated = 0;
+        for (LlamaModel.Output ignored : model.generate(params)) {
+            generated++;
+        }
+        // todo: currently, after generating nPredict tokens, there is an additional empty output
+        Assert.assertTrue(generated > 0 && generated <= nPredict + 1);
+    }
 
-	@Test
-	public void testGenerateGrammar() {
-		InferenceParameters params = new InferenceParameters("")
-				.setGrammar("root ::= (\"a\" | \"b\")+")
-				.setNPredict(nPredict);
-		StringBuilder sb = new StringBuilder();
-		for (LlamaModel.Output output : model.generate(params)) {
-			sb.append(output);
-		}
-		String output = sb.toString();
+    @Test
+    public void testGenerateInfill() {
+        Map<Integer, Float> logitBias = new HashMap<>();
+        logitBias.put(2, 2.0f);
+        InferenceParameters params = new InferenceParameters("")
+                .setInputPrefix(prefix)
+                .setInputSuffix(suffix)
+                .setTemperature(0.95f)
+                .setStopStrings("\"\"\"")
+                .setNPredict(nPredict)
+                .setTokenIdBias(logitBias)
+                .setSeed(42);
 
-		Assert.assertTrue(output.matches("[ab]+"));
-		int generated = model.encode(output).length;
-		Assert.assertTrue(generated > 0 && generated <= nPredict + 1);
-	}
+        int generated = 0;
+        for (LlamaModel.Output ignored : model.generate(params)) {
+            generated++;
+        }
+        Assert.assertTrue(generated > 0 && generated <= nPredict + 1);
+    }
 
-	@Test
-	public void testCompleteAnswer() {
-		Map<Integer, Float> logitBias = new HashMap<>();
-		logitBias.put(2, 2.0f);
-		InferenceParameters params = new InferenceParameters(prefix)
-				.setTemperature(0.95f)
-				.setStopStrings("\"\"\"")
-				.setNPredict(nPredict)
-				.setTokenIdBias(logitBias)
-				.setSeed(42);
+    @Test
+    public void testGenerateGrammar() {
+        InferenceParameters params = new InferenceParameters("")
+                .setGrammar("root ::= (\"a\" | \"b\")+")
+                .setNPredict(nPredict);
+        StringBuilder sb = new StringBuilder();
+        for (LlamaModel.Output output : model.generate(params)) {
+            sb.append(output);
+        }
+        String output = sb.toString();
 
-		String output = model.complete(params);
-		Assert.assertFalse(output.isEmpty());
-	}
+        Assert.assertTrue(output.matches("[ab]+"));
+        int generated = model.encode(output).length;
+        Assert.assertTrue(generated > 0 && generated <= nPredict + 1);
+    }
 
-	@Test
-	public void testCompleteInfillCustom() {
-		Map<Integer, Float> logitBias = new HashMap<>();
-		logitBias.put(2, 2.0f);
-		InferenceParameters params = new InferenceParameters("")
-				.setInputPrefix(prefix)
-				.setInputSuffix(suffix)
-				.setTemperature(0.95f)
-				.setStopStrings("\"\"\"")
-				.setNPredict(nPredict)
-				.setTokenIdBias(logitBias)
-				.setSeed(42);
+    @Test
+    public void testCompleteAnswer() {
+        Map<Integer, Float> logitBias = new HashMap<>();
+        logitBias.put(2, 2.0f);
+        InferenceParameters params = new InferenceParameters(prefix)
+                .setTemperature(0.95f)
+                .setStopStrings("\"\"\"")
+                .setNPredict(nPredict)
+                .setTokenIdBias(logitBias)
+                .setSeed(42);
 
-		String output = model.complete(params);
-		Assert.assertFalse(output.isEmpty());
-	}
+        String output = model.complete(params);
+        Assert.assertFalse(output.isEmpty());
+    }
 
-	@Test
-	public void testCompleteGrammar() {
-		InferenceParameters params = new InferenceParameters("")
-				.setGrammar("root ::= (\"a\" | \"b\")+")
-				.setNPredict(nPredict);
-		String output = model.complete(params);
-		Assert.assertTrue(output.matches("[ab]+"));
-		int generated = model.encode(output).length;
-		Assert.assertTrue(generated > 0 && generated <= nPredict + 1);
-	}
+    @Test
+    public void testCompleteInfillCustom() {
+        Map<Integer, Float> logitBias = new HashMap<>();
+        logitBias.put(2, 2.0f);
+        InferenceParameters params = new InferenceParameters("")
+                .setInputPrefix(prefix)
+                .setInputSuffix(suffix)
+                .setTemperature(0.95f)
+                .setStopStrings("\"\"\"")
+                .setNPredict(nPredict)
+                .setTokenIdBias(logitBias)
+                .setSeed(42);
 
-	@Test
-	public void testEmbedding() {
-		float[] embedding = model.embed(prefix);
-		Assert.assertEquals(4096, embedding.length);
-	}
+        String output = model.complete(params);
+        Assert.assertFalse(output.isEmpty());
+    }
 
-	@Test
-	public void testTokenization() {
-		String prompt = "Hello, world!";
-		int[] encoded = model.encode(prompt);
-		String decoded = model.decode(encoded);
-		// the llama tokenizer adds a space before the prompt
-		Assert.assertEquals(" " + prompt, decoded);
-	}
+    @Test
+    public void testCompleteGrammar() {
+        InferenceParameters params = new InferenceParameters("")
+                .setGrammar("root ::= (\"a\" | \"b\")+")
+                .setNPredict(nPredict);
+        String output = model.complete(params);
+        Assert.assertTrue(output.matches("[ab]+"));
+        int generated = model.encode(output).length;
+        Assert.assertTrue(generated > 0 && generated <= nPredict + 1);
+    }
+
+    @Test
+    public void testEmbedding() {
+        float[] embedding = model.embed(prefix);
+        Assert.assertEquals(4096, embedding.length);
+    }
+
+    @Test
+    public void testTokenization() {
+        String prompt = "Hello, world!";
+        int[] encoded = model.encode(prompt);
+        String decoded = model.decode(encoded);
+        // the llama tokenizer adds a space before the prompt
+        Assert.assertEquals(" " + prompt, decoded);
+    }
 }
